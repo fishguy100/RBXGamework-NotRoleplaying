8aa5b9f2130095403916d2f7e2c88e49e3f9180a
Skye <exp.lunate@gmail.com>
2015-09-28 21:04:03 +0100

    More structure work, because sleep is important.

--- Classes/Stats.mod.lua
+++ Classes/Stats.mod.lua
@@ -3,5 +3,17 @@ local StatsClass = {};
 local StatsMt = {};
 
 return function(Interface)
+  assert(Interface, "Something is wrong: No Interface for Stats", 2);
+  local container = {
+    Attributes = {
 
+    };
+    Mods = {
+
+    };
+    BaseMods = {
+
+    };
+    Interface = Interface;
+  }
 end;
