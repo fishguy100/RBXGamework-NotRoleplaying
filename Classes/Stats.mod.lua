a4a9139b12a7cd11c6ac58dd001eb2428c2ed62f
Skye <exp.lunate@gmail.com>
2015-09-29 19:43:30 +0100

    Stats -> Added Mods, BaseMods & Attributes

--- Classes/Stats.mod.lua
+++ Classes/Stats.mod.lua
@@ -6,13 +6,50 @@ return function(Interface)
   assert(Interface, "Something is wrong: No Interface for Stats", 2);
   local container = {
     Attributes = {
-
+      str = 0;
+      agi = 0;
+      luk = 0;
+      int = 0;
+      cha = 0;
+      dex = 0;
     };
     Mods = {
-
+      HealthCap = 1;
+      ManaCap = 1;
+      EnergyCap = 1;
+      MeleeDefence = 1;
+      RangedDefence = 1;
+      PhysicalDefence = 1;
+      MagicalDefence = 1;
+      FireDefence = 1;
+      IceDefence = 1;
+      PoisonDefence = 1;
+      MeleeAttack = 1;
+      RangedAttack = 1;
+      PhysicalAttack = 1;
+      MagicalAttack = 1;
+      FireAttack = 1;
+      IceAttack = 1;
+      PoisonAttack = 1;
     };
     BaseMods = {
-
+      HealthCap = 1;
+      ManaCap = 1;
+      EnergyCap = 1;
+      MeleeDefence = 1;
+      RangedDefence = 1;
+      PhysicalDefence = 1;
+      MagicalDefence = 1;
+      FireDefence = 1;
+      IceDefence = 1;
+      PoisonDefence = 1;
+      MeleeAttack = 1;
+      RangedAttack = 1;
+      PhysicalAttack = 1;
+      MagicalAttack = 1;
+      FireAttack = 1;
+      IceAttack = 1;
+      PoisonAttack = 1;
     };
     Interface = Interface;
   }
