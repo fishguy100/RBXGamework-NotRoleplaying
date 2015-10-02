89ace3c64d679011919d43b4d4125695e86646e0
Skye <exp.lunate@gmail.com>
2015-10-02 12:55:43 +0000

    Small cleaning up of the link structure in Inventory

--- Classes/Inventory.mod.lua
+++ Classes/Inventory.mod.lua
@@ -3,13 +3,17 @@ local InventoryClass = {};
 local InventoryMt = {};
 
 return function(Interface)
-    local ret = newproxy(true);
-    local mt = getmetatable(ret);
-    InventoryLinks[ret] = {
-
-    };
-    for k,v in next, InventoryMt do
-        mt[k] = v;
-    end;
-    return ret;
+  assert(Interface, "Something is wrong: No Interface supplied for Inventory", 2);
+  local ret = newproxy(true);
+  local mt = getmetatable(ret);
+  InventoryLinks[ret] = {
+    Inventory = {};
+    Equip = {};
+    InventoryQuantity = {};
+    Interface = Interface
+  };
+  for k,v in next, InventoryMt do
+    mt[k] = v;
+  end;
+  return ret;
 end;
